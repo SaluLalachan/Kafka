0590ed971039d5e1ee4025a155c57e1691bb0290
SaluLalachan <151709805+SaluLalachan@users.noreply.github.com>
2023-12-05 16:02:29 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/employees/Employee/Controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.employees.Employee.Controller;
+
+import com.employees.Employee.Services.EmployeeProducer;
+import com.employees.Employee.kafka.avro.model.AddressDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("employee/kafka")
+public class EmployeeController {
+    EmployeeProducer employeeProducer;
+
+    public EmployeeController(EmployeeProducer employeeProducer) {
+        this.employeeProducer = employeeProducer;
+    }
+
+    @PostMapping("/publish")
+    public void sendDetails(@RequestBody AddressDetails addressDetails) throws ExecutionException, InterruptedException {
+        employeeProducer.sendMessage(addressDetails);
+
+    }
+    @PostMapping("/check")
+    public String sendDetails(@RequestParam String message) {
+        //EmployeeProducer.sendMessage(message);
+        return message;
+    }
+}
+
---
+++ Employee/src/main/java/com/employees/Employee/Services/EmployeeConsumer.java
@@ -0,0 +1,38 @@
+package com.employees.Employee.Services;
+
+import com.employees.Employee.kafka.avro.model.AddressDetails;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+public class EmployeeConsumer {
+        public static void main(String[] args) {
+            System.out.println("Consumer invoked");
+            String groupId="employee-data";
+            Properties properties = new Properties();
+            properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+            properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+            properties.setProperty("value.deserializer", KafkaAvroDeserializer.class.getName());
+            properties.setProperty("schema.registry.url","http://localhost/8081");
+            properties.setProperty("group.id",groupId);
+
+            KafkaConsumer<String,AddressDetails> consumer=new KafkaConsumer<>(properties);
+            consumer.subscribe(Arrays.asList("Addressabc"));
+            while(true) {
+                //System.out.println("polling");
+                ConsumerRecords<String, AddressDetails> record = consumer.poll(Duration.ofMillis(1000));
+                for(ConsumerRecord<String,AddressDetails> records: record){
+                    System.out.println(records.value());
+                }
+            }
+        }
+    }
+
+
